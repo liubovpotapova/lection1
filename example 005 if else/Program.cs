af940445dc37a7cca584dea0441f0fbe350a6fed
liubovpotapova <liubov.potapova@gmail.com>
2021-10-27 09:30:31 +0300

    example005 if else

---
+++ example 005 if else/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя пользователя");
+string Username = Console.ReadLine();
+if(Username.ToLower() =="маша")
+{
+    Console .WriteLine ("Машенька, привет!!!");
+}
+else
+{
+    Console .WriteLine ("Привет, ");
+    Console .WriteLine (Username );
+}
