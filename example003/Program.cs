7e9dcbf46aaac49a768e76297309ca412662348e
liubovpotapova <liubov.potapova@gmail.com>
2021-10-27 09:17:37 +0300

    example003 random added

--- example003/Program.cs
+++ example003/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;
-int numberB = 4;
+﻿int numberA = new Random().Next(1, 10);//123456789
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = (numberA+numberB);
 Console.WriteLine(result);
