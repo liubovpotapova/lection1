95af4bb7d771968e9477e68e93e00a26d9dfb332
liubovpotapova <liubov.potapova@gmail.com>
2021-10-28 09:48:15 +0300

    array, while

---
+++ example010 find/Program.cs
@@ -0,0 +1,17 @@
+﻿int [] array = {45,28,57,96,84,85,57,12,42,39};
+
+int n = array.Length;
+int find = 57;
+
+int index = 0;
+
+while(index<n)
+{
+    if(array [index]==find)
+    {
+        Console. WriteLine (index);
+        break ;
+    }
+    //index=index+1;
+    index++;
+}
